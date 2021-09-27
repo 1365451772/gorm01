9764b3c6f326a92cf61e2c86e98fdae2d17f5ad7
sunpeng <sunpeng@example.com>
2021-09-27 14:55:36 +0800

    go 数据库查询

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,36 @@
-package cmd
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+type BannerBook struct {
+	Name         string `json:"name" gorm:"column:book_name"`
+	ActionType   int    `json:"actionType gorm:"column:action_type"`
+	BannerUrl    string `json:"bannerUrl" gorm:"column:url"`
+	Introduction string `json:"introduction" gorm:"column:introduction"`
+	BookId       int    `json:"bookId" gorm:"column:book_id"`
+}
 
 func main() {
-	fmt.Print(111)
+	fmt.Print("数据库开始连接\n")
+	db, err := gorm.Open(
+		"mysql",
+		"root:peng1365451772@/novel?charset=utf8&parseTime=True&loc=Local",
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+	var banner []BannerBook
+	db.Table("read_ws_banner").Select("book_name,action_type,url,introduction,book_id").Where("app_id = 'com.fantasy.best.novel'").Order("sort").Scan(&banner)
+	fmt.Println(db)
+	fmt.Println(banner)
+	var names []string
+	db.Table("read_ws_banner").Pluck("book_name", &names)
+
+	fmt.Println(names)
 
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/1365451772/gorm01.git
 
 go 1.16
+
+require github.com/jinzhu/gorm v1.9.16
